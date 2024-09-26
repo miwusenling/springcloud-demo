7ba6cfddf214d500232d487ec8d7e278abd1a59b
miwusenling <38873352+miwusenling@users.noreply.github.com>
2024-09-26 14:26:01 +0800

    Update EurekaApplication.java
    test llm code review

--- springcloud-parent/springcloud-eureka-server/src/main/java/com/srpringcloud/eureka/EurekaApplication.java
+++ springcloud-parent/springcloud-eureka-server/src/main/java/com/srpringcloud/eureka/EurekaApplication.java
@@ -8,6 +8,13 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
+
+	public static void test_1(){
+		int a = 10;
+		int b = 0;
+		int c = a / b;
+		System.out.println(c);
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
