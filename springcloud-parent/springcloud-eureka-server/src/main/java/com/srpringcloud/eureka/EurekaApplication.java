aee05c418f6302f345219c088a4c61a2008433ee
miwusenling <38873352+miwusenling@users.noreply.github.com>
2024-09-29 10:42:31 +0800

    Update EurekaApplication.java
    test llm code review

--- springcloud-parent/springcloud-eureka-server/src/main/java/com/srpringcloud/eureka/EurekaApplication.java
+++ springcloud-parent/springcloud-eureka-server/src/main/java/com/srpringcloud/eureka/EurekaApplication.java
@@ -15,6 +15,13 @@ public class EurekaApplication {
 		int c = a / b;
 		System.out.println(c);
 	}
+
+	public static void test_2(){
+		int a = 10;
+		int b = 0;
+		int c = a / b;
+		System.out.println(c);
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
